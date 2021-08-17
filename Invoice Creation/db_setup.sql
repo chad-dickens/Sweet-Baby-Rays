b48b6abd31a04afb0a069eb96f803f62d836c13a
Chad Dickens <61530410+chad-dickens@users.noreply.github.com>
2021-08-17 22:24:38 +1000

    Add files via upload

--- Invoice Creation/db_setup.sql
+++ Invoice Creation/db_setup.sql
@@ -1,44 +1,53 @@
+DROP TABLE IF EXISTS address_data;
+DROP TABLE IF EXISTS rates_data;
+DROP TABLE IF EXISTS sales_data;
+
 CREATE TABLE address_data (
-ID int NOT NULL AUTO_INCREMENT, 
-Country_Code CHAR(2),
-Physical_Address VARCHAR(200),
-Country_Name VARCHAR(50),
-Email_Address VARCHAR(50),
-PRIMARY KEY(ID));
+	Country_Code CHAR(2) PRIMARY KEY,
+    Physical_Address VARCHAR(200),
+    Country_Name VARCHAR(50),
+    Email_Address VARCHAR(50)
+);
 
 CREATE TABLE rates_data (
-ID int NOT NULL AUTO_INCREMENT, 
-Rate_Reference CHAR(11),
-Operator CHAR(3),
-Despatch_Year SMALLINT,
-Sub_Account_Type VARCHAR(50),
-PL CHAR(1),
-Country_Code CHAR(2),
-Mail_Category CHAR(1),
-Subclass CHAR(2),
-Rate_Ltr_Kg DECIMAL(8,4),
-Rate_Ltr_Itm DECIMAL(8,4),
-Rate_Bulk_Kg DECIMAL(8,4),
-Rate_Bulk_Itm DECIMAL(8,4),
-PRIMARY KEY(ID));
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Rate_Reference CHAR(11),
+    Operator CHAR(3),
+    Despatch_Year SMALLINT,
+    Sub_Account_Type VARCHAR(50),
+    PL CHAR(1),
+    Country_Code CHAR(2),
+    Mail_Category CHAR(1),
+    Subclass CHAR(2),
+    Rate_Ltr_Kg DECIMAL(8 , 4 ),
+    Rate_Ltr_Itm DECIMAL(8 , 4 ),
+    Rate_Bulk_Kg DECIMAL(8 , 4 ),
+    Rate_Bulk_Itm DECIMAL(8 , 4 ),
+    CONSTRAINT fk_rates_country FOREIGN KEY (Country_Code)
+        REFERENCES address_data (Country_Code)
+        ON DELETE RESTRICT
+);
 
 CREATE TABLE sales_data (
-ID int NOT NULL AUTO_INCREMENT, 
-Sub_Account_Type VARCHAR(50),
-Despatch_Year SMALLINT,
-Despatch_Month TINYINT,
-Despatch_Date DATE,
-Qtr TINYINT,
-Despatch_ID VARCHAR(30),
-Serial_Number INT,
-Origin CHAR(6),
-Destination CHAR(6),
-Mail_Category CHAR(1),
-Class CHAR(1),
-Subclass CHAR(2),
-PL CHAR(2),
-Operator CHAR(3),
-Country_Code CHAR(2),
-No_of_ItRates INT,
-Weight_Kgs DECIMAL(10,4),
-PRIMARY KEY(ID));
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Sub_Account_Type VARCHAR(50),
+    Despatch_Year SMALLINT,
+    Despatch_Month TINYINT,
+    Despatch_Date DATE,
+    Qtr TINYINT,
+    Despatch_ID VARCHAR(30),
+    Serial_Number INT,
+    Origin CHAR(6),
+    Destination CHAR(6),
+    Mail_Category CHAR(1),
+    Class CHAR(1),
+    Subclass CHAR(2),
+    PL CHAR(2),
+    Operator CHAR(3),
+    Country_Code CHAR(2),
+    No_of_ItRates INT,
+    Weight_Kgs DECIMAL(10 , 4 ),
+    CONSTRAINT fk_sales_country FOREIGN KEY (Country_Code)
+        REFERENCES address_data (Country_Code)
+        ON DELETE RESTRICT
+);
